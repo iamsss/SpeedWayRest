f266fb2ee0dd500da1aa618a4a29d87edd1c8247
iamsss <saurav9760@gmail.com>
2020-10-29 11:25:35 +0530

    Adding Versioning Of Api

---
+++ SpeedWayRest/Controllers/V1/PostsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpeedWayRest.Contracts;
+using SpeedWayRest.Domain;
+
+namespace SpeedWayRest.Controllers
+{
+    public class PostsController : Controller
+    {
+        private List<Post> _Posts = new List<Post>();
+        public PostsController()
+        {
+            _Posts.Add(new Post("First Post"));
+            _Posts.Add(new Post("Second Post"));
+            _Posts.Add(new Post("Thrid Post"));
+            _Posts.Add(new Post("Fourth Post"));
+
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult Index()
+        {
+            return Ok(_Posts);
+        }
+    }
+}
---
+++ SpeedWayRest/Domain/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeedWayRest.Domain
+{
+    public class Post
+    {
+
+        public string Id { get; set; }
+        public Post(string id)
+        {
+            this.Id = id;
+        }
+
+    }
+}
